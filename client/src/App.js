e75950b0fbd12126ec0bc67f95b11f242ca9ebce
fahmihafiz <fahmimoon13@gmail.com>
2024-04-15 03:11:37 +0800

    Resolve conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import DragDrop from "./Component/DragDrop";
 import "./App.css";
-import Box from "@mui/material/Box";
-import Result from "./Component/Result";
 
 function App() {
   const [result, setResult] = useState(null);
