a6c9248d15ed6bbdd2b60c6f884086c31937a627
fahmihafiz <fahmimoon13@gmail.com>
2024-04-17 09:32:17 +0800

    Remove duplicated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
-import DragDrop from "./Component/DragDrop";
-import UI from "./Component/UI";
 import "./App.css";
 import NavBar from "./Component/NavBar";
-import Test from "./Component/Test";
+import DragDrop from "./Component/DragDrop";
 
 function App() {
   return (
     <div>
       <NavBar/>
-     <Test/>
+     <DragDrop/>
     </div>
   );
 }
